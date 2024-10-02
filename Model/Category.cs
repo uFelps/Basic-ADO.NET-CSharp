d16966716542df2951424068c37b7279281e1c07
uFelps <felipesand824@gmail.com>
2024-10-02 12:26:19 -0300

    feat: crud with Dapper

---
+++ Model/Category.cs
@@ -0,0 +1,28 @@
+namespace dataAccess.Model;
+
+public class Category
+{
+    public Guid? Id { get; set; }
+    public string? Title { get; set; }
+    public string? Url { get; set; }
+    public string? Summary { get; set; }
+    public int? Order { get; set; }
+    public string? Description { get; set; }
+    public bool? Featured { get; set; }
+
+    public Category()
+    {
+        
+    }
+
+    public Category(Guid id, string title, string url, string summary, int order, string description, bool featured)
+    {
+        Id = id;
+        Title = title;
+        Url = url;
+        Summary = summary;
+        Order = order;
+        Description = description;
+        Featured = featured;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,70 @@
 ﻿using System.Data;
+using Dapper;
+using dataAccess.Model;
 using Microsoft.Data.SqlClient;
 
-const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
+const string connectionString =
+    "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
 
-using (var connetion = new SqlConnection(connectionString))
+
+using (var connection = new SqlConnection(connectionString))
 {
-    Console.WriteLine("Conectado");
-    connetion.Open();
-
-    using (var command = new SqlCommand())
-    {
-        command.Connection = connetion;
-        command.CommandType = CommandType.Text;
-        command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-        var reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            Console.WriteLine($"{reader.GetGuid(0)}, {reader.GetString(1)}");
-        }
-    }
-    
-    connetion.Close();
-    
+    //ListCategories(connection);
+    //CreateCategory(connection);
+    //UpdateCategory(connection);
+    //DeleteCategory(connection);
 }
+
+
+static void ListCategories(SqlConnection connection)
+{
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Id}, {category.Title}");
+    }
+}
+
+static void CreateCategory(SqlConnection connection)
+{
+    var newcategory = new Category(new Guid(),
+        "Amazon AWS", "amazon.com", "AWS Cloud", 8,
+        "Categoria destinada aos serviços aws", false);
+
+    var insertSql = "INSERT INTO [Category] VALUES(@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
+    var rolls = connection.Execute(insertSql, new
+    {
+        newcategory.Id, newcategory.Title, newcategory.Url, newcategory.Summary,
+        newcategory.Order, newcategory.Description, newcategory.Featured
+    });
+
+    Console.WriteLine($"{rolls} Linhas Inseridas");
+}
+
+static void UpdateCategory(SqlConnection connection)
+{
+    var updateSql = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
+
+    var rolls = connection.Execute(updateSql, new
+    {
+        id = "00000000-0000-0000-0000-000000000000",
+        title = "Azure"
+    });
+
+    Console.WriteLine($"{rolls} Linhas Atualizadas");
+}
+
+static void DeleteCategory(SqlConnection connection)
+{
+    var deleteSql = "DELETE [Category] WHERE [Title] LIKE @title";
+
+    var rolls = connection.Execute(deleteSql, new
+    {
+        title = "Azure"
+    });
+
+    Console.WriteLine($"{rolls} Linhas deletadas");
+}
+
